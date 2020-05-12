648075e9cdae3b331d5e65712a1517eccb50928d
Xcorpiion <vinicius.da.silva.cruz,sanji@gmail.com>
2020-05-12 00:09:31 -0300

    Criando a model Categorias e alterando os dados do CategoriaResource

---
+++ src/main/java/br/com/estudo/projetoweb/domain/Categoria.java
@@ -0,0 +1,68 @@
+package br.com.estudo.projetoweb.domain;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+
+    private static  final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof Categoria)) return false;
+
+        Categoria categoria = (Categoria) o;
+
+        return new EqualsBuilder()
+                .append(id, categoria.id)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("id", id)
+                .append("nome", nome)
+                .toString();
+    }
+}
--- src/main/java/br/com/estudo/projetoweb/resources/CategoriaResource.java
+++ src/main/java/br/com/estudo/projetoweb/resources/CategoriaResource.java
@@ -1,16 +1,26 @@
 package br.com.estudo.projetoweb.resources;
 
+import br.com.estudo.projetoweb.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String listar() {
-        return "Rest está funcionando";
+    public List<Categoria> listar() {
+
+        Categoria categoria1 = new Categoria(1L, "Informatica");
+        Categoria categoria2 = new Categoria(2L, "Escritorio");
+        List<Categoria> categorias = new ArrayList<>();
+        categorias.add(categoria1);
+        categorias.add(categoria2);
+        return categorias;
     }
 
 }
