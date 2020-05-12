5142fd9c83fd3706e866562422856f445a0e0dec
Xcorpiion <vinicius.da.silva.cruz,sanji@gmail.com>
2020-05-12 00:19:43 -0300

    Mappeando a classe categoria e configurando application.properties

--- src/main/java/br/com/estudo/projetoweb/domain/Categoria.java
+++ src/main/java/br/com/estudo/projetoweb/domain/Categoria.java
@@ -4,12 +4,19 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class Categoria implements Serializable {
 
     private static  final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
