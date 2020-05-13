e608965921714c457abeeae5045720de019e00af
Xcorpiion <vinicius.da.silva.cruz,sanji@gmail.com>
2020-05-13 00:28:56 -0300

    Inicializando os metodos usando o CommandLineRunner

--- src/main/java/br/com/estudo/projetoweb/ProjetoWebApplication.java
+++ src/main/java/br/com/estudo/projetoweb/ProjetoWebApplication.java
@@ -1,13 +1,30 @@
 package br.com.estudo.projetoweb;
 
+import br.com.estudo.projetoweb.domain.Categoria;
+import br.com.estudo.projetoweb.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+
+import java.util.Arrays;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoWebApplication {
+public class ProjetoWebApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoWebApplication.class, args);
-	}
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoWebApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria categoria1 = new Categoria(null, "Informática");
+        Categoria categoria2 = new Categoria(null, "Escritorio");
+
+        categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+    }
 }
